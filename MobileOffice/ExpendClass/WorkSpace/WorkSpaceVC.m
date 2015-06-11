329492857b3cf34b62b68a0a4ac58fe1aba817da
feihuohua <feihuohua@163.com>
2015-06-11 12:48:42 +0800

    [wang]增加首页中滑动的segment控件

--- MobileOffice/ExpendClass/WorkSpace/WorkSpaceVC.m
+++ MobileOffice/ExpendClass/WorkSpace/WorkSpaceVC.m
@@ -7,8 +7,10 @@
 //
 
 #import "WorkSpaceVC.h"
+#import "LXSegmentView.h"
 
 @interface WorkSpaceVC ()
+@property (nonatomic, strong) LXSegmentView *mySegmentView;
 
 @end
 
@@ -18,6 +20,20 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
     self.navigationItem.title = @"我的文档";
+}
+
+- (void)initUI{
+    
+    self.automaticallyAdjustsScrollViewInsets = NO;
+    
+    self.mySegmentView = [[LXSegmentView alloc] initWithFrame:CGRectMake(0, 64, kScreenWidth, kScreenHeight-64-49)];
+    self.mySegmentView.tabBgImageView.image = [UIImage imageNamed:@"bg_tab_selected.png"];
+    self.mySegmentView.tabButtonSeclectImageView.image = [UIImage imageNamed:@"select_flag.png"];
+    self.mySegmentView.tabButtonColor = [UIColor blackColor];
+    self.mySegmentView.tabButtonSelectCorlor = [UIColor redColor];
+    [self.mySegmentView setTabButton1Title:@"我上传的" andButton2Title:@"我分享的" andButton3Title:@"分享给我的"];
+    
+    [self.view addSubview:self.mySegmentView];
 }
 
 - (void)didReceiveMemoryWarning {
