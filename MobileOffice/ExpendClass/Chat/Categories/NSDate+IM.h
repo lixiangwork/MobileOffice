b52877c6a82d4ff27f91b9bfc379f80e39af84dc
lionryin <email>
2015-08-11 15:24:23 +0800

    [lx]add NSDate+IM

---
+++ MobileOffice/ExpendClass/Chat/Categories/NSDate+IM.h
@@ -0,0 +1,30 @@
+//
+//  NSDate+IM.h
+//  JLWeChat
+//
+//  Created by jimneylee on 14-5-22.
+//  Copyright (c) 2014年 jimneylee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#define WEEK_DAYS @[@"星期日", @"星期一", @"星期二", @"星期三", @"星期四", @"星期五", @"星期六"]
+
+@interface NSDate (IM)
+
+// yyyy-MM-dd HH:mm:ss
++ (NSDate *)formatLongDateTimeFromString:(NSString *)string;
+
+- (NSString *)formatRecentContactDate;
+
+- (NSString *)formatChatMessageDate;
+
+- (BOOL)isToday;
+
+- (BOOL)isYesterday;
+
+- (BOOL)isTomorrow;
+
+- (BOOL)isInWeek;
+
+@end
